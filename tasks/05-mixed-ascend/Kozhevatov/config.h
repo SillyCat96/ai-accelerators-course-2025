1b383674d984e3ddf6c23234d229fcda6de3d694
Lesha <alexey.kozhevatov@mail.ru>
2026-01-31 04:47:09 +0300

    Fix formatting of config.h

--- tasks/05-mixed-ascend/Kozhevatov/config.h
+++ tasks/05-mixed-ascend/Kozhevatov/config.h
@@ -17,14 +17,14 @@
 #define BLOCK_LENGTH (TOTAL_LENGTH / USE_CORE_NUM)  // 9216 / 96 = 96
 
 // Для MatMul: 2 ядра делят матрицу 96x96 на куски по 48 столбцов
-#define MATMUL_CORE_NUM 2 
+#define MATMUL_CORE_NUM 2
 
 // 3. Параметры активации
 #define LEAKY_RELU_ALPHA 0.001f
 
 // 5. Константы для Softmax
-#define BUFFER_NUM 2 // Двойная буферизация
+#define BUFFER_NUM 2  // Двойная буферизация
 #define TILE_NUM 1
 #define TILE_LENGTH (BLOCK_LENGTH / TILE_NUM / BUFFER_NUM)  // 96 / 2 / 1 = 48
 
-#endif // CONFIG_H
+#endif  // CONFIG_H
